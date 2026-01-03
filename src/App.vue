3fc200b1137a0b2392e6e735b946d30aa3c6bd17
wingedseraph <r89647@gmail.com>
2026-01-03 18:10:09 +0300

    feat(app-view): implement transition api to route change

--- src/App.vue
+++ src/App.vue
@@ -3,73 +3,51 @@ import { RouterView } from 'vue-router';
 </script>
 
 <template>
-  <header>
-    <div class="wrapper"></div>
-  </header>
-
-  <RouterView />
+  <RouterView v-slot="{ Component }">
+    <transition name="route" mode="out-in">
+      <component :is="Component" />
+    </transition>
+  </RouterView>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<style>
+#app {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem;
+  box-sizing: border-box;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.route-enter-from {
+  opacity: 0;
+  transform: translateX(100px);
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+.route-leave-to {
+  opacity: 0;
+  transform: translateX(-100px);
 }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+.route-enter-active,
+.route-leave-active {
+  transition: all 0.3s ease;
 }
 
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.greetings {
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
 }
 </style>
