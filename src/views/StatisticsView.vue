be23e635072cbb81230a76f175fffb51006d35ae
wingedseraph <r89647@gmail.com>
2026-01-06 00:26:36 +0300

    chore(statictic-view): remove h1, use title instead

--- src/views/StatisticsView.vue
+++ src/views/StatisticsView.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
 import LayoutDefault from '@/components/LayoutDefault.vue';
-
 </script>
 
 <template>
   <LayoutDefault title="Statistics" :centered="true">
     <div class="statistics">
-      <h1>Game Statistics</h1>
       <p>Your game progress and achievements will be displayed here.</p>
     </div>
   </LayoutDefault>
